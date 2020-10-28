20addfba79f93d30ea850e30d89008dc0ff18452
alidzen <d.s.golubtsov@gmail.com>
2020-10-28 13:20:56 +0100

    [sprint 11, part3] Add correct permissions

--- socials/views.py
+++ socials/views.py
@@ -4,17 +4,13 @@ from rest_framework import viewsets
 
 from category.models import Titles
 from socials.models import Review
-from socials.permissions import IsAuthorOrReadOnly
+from socials.permissions import IsAuthorOrReadOnly, IsAdminOrIsModerator
 from socials.serializers import ReviewSerializer, CommentSerializer
 
 
-# TODO:
-#  1. change response to NO paginated data
-#  2. change permission from IsAuthorOrReadOnly to IsAuthorOrReadOnly and IsModerator and IsAdmin
-
 class ReviewView(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    # permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = [IsAuthorOrReadOnly or IsAdminOrIsModerator]
 
     def perform_create(self, serializer):
         title = get_object_or_404(Titles, pk=self.kwargs.get('title_id'))
@@ -27,7 +23,7 @@ class ReviewView(viewsets.ModelViewSet):
 
 class CommentView(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    # permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = [IsAuthorOrReadOnly or IsAdminOrIsModerator]
 
     def perform_create(self, serializer):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
