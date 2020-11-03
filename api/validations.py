ad53e1421a34c8c1503b00e75960d46d9e09557f
Noskov Alexey <vokson@yandex.ru>
2020-11-03 05:51:53 +0300

    fix-05 finished

--- api/validations.py
+++ api/validations.py
@@ -7,8 +7,9 @@ from api.models import Review, Title
 def uniq_review(context, data):
     request = context.get('request')
     author = request.user
-    view = context.get('view')
-    title_id = view.kwargs.get('title_id')
+    # view = context.get('view')
+    # title_id = view.kwargs.get('title_id')
+    title_id = request.parser_context['kwargs']['title_id']
     title = get_object_or_404(Title, pk=title_id)
 
     if Review.objects.filter(title=title, author=author).exists():
